ec4a564caff5b49b5ce80fd8c9ac57eb2c64c8f9
SergeiLeshchev <ls89653267617@yandex.ru>
2025-07-05 17:48:28 +0200

    Убрать неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/filmorate/validator/FilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/FilmValidator.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.time.LocalDate;
 
